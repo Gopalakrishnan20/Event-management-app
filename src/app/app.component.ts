c8671bea8cc9ff24eb05693e4e37e12a4674c65e
Krishna <gopalakrishnan567890@gmail.com>
2022-09-22 10:43:14 +0530

    1.Prototype

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CrudHttpService } from './crud-http.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Event-management-app';
+  todoList:any = [];
+
+  constructor(private crudHttpService: CrudHttpService){}
+
+  ngOnInit(): void {
+    this.listTodos();
+  }
+
+  //to display all records
+  listTodos(){
+    this.crudHttpService.list().subscribe((response)=>{
+      this.todoList = response;
+    },(error=>{
+
+    }));
+  }
+
+  //create new record
+  createTodo(){
+    let todo = {
+      id: new Date().getTime(),
+      title:`Some Todo` 
+    }
+    this.crudHttpService.create(todo).subscribe((response)=>{
+      this.listTodos();
+    },(error=>{
+
+    }));
+  }
+
+  //modify record
+  editTodo(todo: any){
+    let data = {
+      id: new Date().getTime(),
+      title:`modify` 
+    }
+    this.crudHttpService.update(todo.id,data).subscribe((response)=>{
+      this.listTodos();
+    },(error=>{
+
+    }));
+  }
+
+  //delete a record
+  deleteTodo(id: any){
+    this.crudHttpService.delete(id).subscribe((response)=>{
+      this.listTodos();
+    },(error=>{
+    }));
+  }
 }
